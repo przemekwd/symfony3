71f53c96842edbbce3dae752d9d740c770f0352b
Przemysław Mincewicz <p.mincewicz@smarteducation.pl>
2017-07-18 11:51:07 +0200

    Fix main menu and add user menu

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -3,26 +3,58 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class MenuBuilder
 {
     private $factory;
+    private $tokenStorage;
 
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, TokenStorage $tokenStorage)
     {
         $this->factory = $factory;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function createMainMenu(array $options)
     {
         $menu = $this->factory->createItem('root', [
             'childrenAttributes' => [
-                'class' => 'nav navbar-nav',
+                'class' => 'nav navbar-nav navbar-left',
             ],
         ]);
         $menu->addChild('Home', ['route' => 'homepage']);
         $menu->addChild('Games', ['route' => 'game_index']);
         $menu->addChild('Developers', ['route' => 'developer_index']);
+
+        return $menu;
+    }
+
+    public function createUserMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root', [
+            'childrenAttributes' => [
+                'class' => 'nav navbar-nav navbar-right',
+            ],
+        ]);
+
+        if ($token = $this->tokenStorage->getToken()->getRoles()) {
+            $menu->addChild($this->tokenStorage->getToken()->getUsername(), ['route' => 'homepage']);
+            $menu->addChild('Logout', [
+                'uri' => '/logout',
+                'attributes' => [
+                    'class' => 'usermenu-last'
+                ],
+            ]);
+        } else {
+            $menu->addChild('Login', ['uri' => '/login']);
+            $menu->addChild('Register', [
+                'uri' => '/register',
+                'attributes' => [
+                    'class' => 'usermenu-last'
+                ],
+            ]);
+        }
 
         return $menu;
     }
